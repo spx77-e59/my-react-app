35931f397cdd0c4310a6c6c10ca18bfca6808e0a
somtsho <179671017+spx77-e59@users.noreply.github.com>
2024-10-19 19:32:37 +0600

    Handle bad urls

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Oh no, this route doesn&apos;t exist!</h1>
+      <Link>You can go back to the home page by clicking here, though!</Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
-import Profile from "./Profile.jsx";
-import Spinach from "./Spinach.jsx";
-import Popeye from "./Popeye.jsx";
-import DefaultProfile from "./DefaultProfile.jsx";
+import routes from "./routes";
 import "./index.css";
 
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  {
-    path: "profile",
-    element: <Profile />,
-    children: [
-      { index: true, element: <DefaultProfile /> },
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(routes);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
